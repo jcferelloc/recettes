a8a6be702d36d403ae59c554a424626be16cde54
JCFerelloc <jean-christophe.ferelloc@al-enterprise.com>
2017-12-19 13:10:08 +0100

    Fix UTF-8 issue when fill recette

--- src/updateRecette.php
+++ src/updateRecette.php
@@ -4,22 +4,29 @@ $params = $_POST;
 if (sizeof($_POST)==0){
 	$params = $_GET;
 }
+/*
 var_dump($_POST);
 var_dump($_GET);
 var_dump($params);
+*/
+
+function textTreatment($text){
+    return utf8_decode (htmlspecialchars(str_replace("'","\\'",$text)));
+}
 
 $action = htmlspecialchars($params["action"]);
 
 if ( $action != "delete"){
     $id = htmlspecialchars($params["id"]);
-    $nom = htmlspecialchars($params["nom"]);
+    $nom = textTreatment($params["nom"]);
     $categorie = htmlspecialchars($params["categorie"]);
-    $titre = htmlspecialchars($params["titre"]);
-    $presentation = htmlspecialchars($params["presentation"]);
-    $ingredients = htmlspecialchars($params["ingredients"]);
-    $preparation = htmlspecialchars($params["preparation"]);
-    $indications = htmlspecialchars($params["indications"]);
+    $titre = textTreatment($params["titre"]);
+    $presentation = textTreatment($params["presentation"]);
+    $ingredients = textTreatment($params["ingredients"]);
+    $preparation = textTreatment($params["preparation"]);
+    $indications = textTreatment($params["indications"]);
 }
+
 
 include 'connect.php';
 $connection = connect();
@@ -57,7 +64,7 @@ if ( $action == "new" ){
     $query .= "preparation = '" . $preparation . "', ";
     $query .= "indications = '" . $indications  . "' ";
     $query .= "WHERE id = " . $id . ";";
-
+var_dump($query);
     $result = $connection->query( $query );
 }
 if ( ! $result ){
